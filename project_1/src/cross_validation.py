7800b9329e7b1601df93c0bf306f6eaf2eff1837
Nicholas Karlsen <nicholas.karlsen@fys.uio.no>
2020-09-21 13:29:45 +0200

    Added cross-validation in notebook + unit test for Ridge

---
+++ project_1/src/cross_validation.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+import stat_tools
+import linear_regression
+
+def cross_validation(X, z, k_folds, regression=linear_regression.OLS_SVD_2D):
+
+    columns = [
+        "MSE train", "MSE test",
+        "R2 train", "R2 test"
+        #"Variance train", "Variance test"
+        #"Bias train", "Bias test"
+    ]
+    dat = pd.DataFrame(columns=columns, index=np.arange(k_folds))
+    test_indices, train_indices = stat_tools.k_fold_selection(z, k = k_folds)
+
+    for k in range(k_folds):
+        # Training data
+        X_train = X[train_indices[k], :]
+        z_train = z[train_indices[k]]
+        # Testing data
+        X_test = X[test_indices[k], :]
+        z_test = z[test_indices[k]]
+
+        beta = regression(X_train, z_train)
+
+        dat["MSE train"][k] = stat_tools.MSE(z_train, X_train @ beta)
+        dat["R2 train"][k] = stat_tools.R2(z_train, X_train @ beta)
+
+        dat["MSE test"][k] = stat_tools.MSE(z_test, X_test @ beta)
+        dat["R2 test"][k] = stat_tools.R2(z_test, X_test @ beta)
+
+    return dat
--- project_1/src/unit_tests.py
+++ project_1/src/unit_tests.py
@@ -1,24 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
 
 import linear_regression
 from main import FrankeFunction
-    
+
 
 def OLS_unit_test(min_deg=2, max_deg=5, tol=1e-6):
-    n = 100 # Number of data points
+    n = 100  # Number of data points
     # Prepare data set
     x = np.random.uniform(0, 1, n)
     y = np.random.uniform(0, 1, n)
     z = FrankeFunction(x, y) + np.random.normal(0, 1, n) * 0.2
-    degrees = np.arange(min_deg, max_deg+1)
+    degrees = np.arange(min_deg, max_deg + 1)
     for deg in degrees:
         # Set up design matrix
         X = linear_regression.design_matrix_2D(x, y, 5)
         # Compute optimal parameters using our homegrown OLS
-        beta = linear_regression.OLS_2D(X = X, z = z)
+        beta = linear_regression.OLS_2D(X=X, z=z)
         # Compute optimal parameters using sklearn
         skl_reg = LinearRegression(fit_intercept=False).fit(X, z)
         beta_skl = skl_reg.coef_
@@ -27,7 +27,9 @@ def OLS_unit_test(min_deg=2, max_deg=5, tol=1e-6):
             if abs(beta[i] - beta_skl[i]) < tol:
                 pass
             else:
-                print("Warning! mismatch with SKL in OLS_unit_test with tol = %.0e" % tol)
+                print(
+                    "Warning! mismatch with SKL in OLS_unit_test with tol = %.0e" % tol
+                )
                 print("Parameter no. %i for deg = %i" % (i, deg))
                 print("-> (OUR) beta = %8.12f" % beta[i])
                 print("-> (SKL) beta = %8.12f" % beta_skl[i])
@@ -35,17 +37,17 @@ def OLS_unit_test(min_deg=2, max_deg=5, tol=1e-6):
 
 
 def OLS_SVD_unit_test(min_deg=2, max_deg=5, tol=1e-6):
-    n = 100 # Number of data points
+    n = 100  # Number of data points
     # Prepare data set
     x = np.random.uniform(0, 1, n)
     y = np.random.uniform(0, 1, n)
     z = FrankeFunction(x, y) + np.random.normal(0, 1, n) * 0.2
-    degrees = np.arange(min_deg, max_deg+1)
+    degrees = np.arange(min_deg, max_deg + 1)
     for deg in degrees:
         # Set up design matrix
         X = linear_regression.design_matrix_2D(x, y, 5)
         # Compute optimal parameters using our homegrown OLS
-        beta = linear_regression.OLS_SVD_2D(X = X, z = z)
+        beta = linear_regression.OLS_SVD_2D(X=X, z=z)
         # Compute optimal parameters using sklearn
         skl_reg = LinearRegression(fit_intercept=False).fit(X, z)
         beta_skl = skl_reg.coef_
@@ -54,13 +56,48 @@ def OLS_SVD_unit_test(min_deg=2, max_deg=5, tol=1e-6):
             if abs(beta[i] - beta_skl[i]) < tol:
                 pass
             else:
-                print("Warning! mismatch with SKL in OLS_SVD_unit_test with tol = %.0e" % tol)
+                print(
+                    "Warning! mismatch with SKL in OLS_SVD_unit_test with tol = %.0e"
+                    % tol
+                )
                 print("Parameter no. %i for deg = %i" % (i, deg))
                 print("-> (OUR) beta = %8.12f" % beta[i])
                 print("-> (SKL) beta = %8.12f" % beta_skl[i])
     return
 
-    
+
+def Ridge_2D_unit_test(min_deg=2, max_deg=5, tol=1e-6):
+    n = 100  # Number of data points
+    # Prepare data set
+    x = np.random.uniform(0, 1, n)
+    y = np.random.uniform(0, 1, n)
+    z = FrankeFunction(x, y) + np.random.normal(0, 1, n) * 0.2
+    degrees = np.arange(min_deg, max_deg + 1)
+    for deg in degrees:
+        # Set up design matrix
+        X = linear_regression.design_matrix_2D(x, y, 5)
+        for lamb in np.linspace(0, 1, 10):
+            # Compute optimal parameters using our homegrown Ridge regression
+            beta = linear_regression.Ridge_2D(X=X, z=z, lamb=lamb)
+            # Compute optimal parameters using sklearn
+            skl_reg = Ridge(alpha=lamb, fit_intercept=False).fit(X, z)
+            beta_skl = skl_reg.coef_
+
+            for i in range(len(beta)):
+                if abs(beta[i] - beta_skl[i]) < tol:
+                    pass
+                else:
+                    print(
+                        "Warning! mismatch with SKL in Ridge_2D_unit_test with tol = %.0e"
+                        % tol
+                    )
+                    print("Parameter no. %i for deg = %i" % (i, deg))
+                    print("-> (OUR) beta = %8.12f" % beta[i])
+                    print("-> (SKL) beta = %8.12f" % beta_skl[i])
+    return
+
+
 if __name__ == "__main__":
     OLS_unit_test()
     OLS_SVD_unit_test()
+    Ridge_2D_unit_test()
