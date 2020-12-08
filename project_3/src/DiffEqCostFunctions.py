a985e63dbfe7c5deb3c6844525de89f9caea8854
Nicholas <nicholas.karlsen@fys.uio.no>
2020-12-08 12:47:27 +0100

    added some notebooks & a bit to the report

---
+++ project_3/src/DiffEqCostFunctions.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+import sys
+sys.path.insert(0, "../../project_2/src")
+
+from CostFunctions import *
+
+
+class HeatEquationCost(CostFunction):
+	def __init__(self):
+		# pass the trial function etc
+		pass
+
